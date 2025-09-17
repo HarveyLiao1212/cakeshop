3ae6c135187fb89637702471ff524afaed114cd7
2500533 <uloverharvey@icloud.com>
2025-09-17 15:17:03 +0800

    商品功能 - 新增功能 修改商品 刪除商品

--- src/main/java/com/harvey/cakeshop/controller/ProductController.java
+++ src/main/java/com/harvey/cakeshop/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ProductController {
@@ -15,8 +16,10 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/products/{productId}") // 查詢商品
-    public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
+    // 查詢商品
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<Product> getProduct(
+            @PathVariable Integer productId) {
 
         Product product = productService.getProductById(productId);
 
@@ -27,15 +30,35 @@ public class ProductController {
         }
     }
 
+    // 新增商品
     @PostMapping("/products")
     public ResponseEntity<Product> createProduct(
             @RequestBody @Valid ProductCreateRequest productCreateRequest) {
 
         Integer productId = productService.createProduct(productCreateRequest);
-
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    // 修改商品
+    @PostMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Integer productId,
+            @RequestBody @Valid ProductCreateRequest productCreateRequest) {
+
+        // 修改商品數據
+        Product updatedProduct = productService.updateProduct(productId,productCreateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
+
+    // 刪除商品
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(
+            @PathVariable Integer productId) {
+
+        productService.deleteProduct(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
 }
--- src/main/java/com/harvey/cakeshop/service/ProductService.java
+++ src/main/java/com/harvey/cakeshop/service/ProductService.java
@@ -5,7 +5,11 @@ import com.harvey.cakeshop.dto.product.ProductCreateRequest;
 import com.harvey.cakeshop.model.Product;
 import com.harvey.cakeshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 
 @Service
@@ -14,12 +18,13 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    // 查詢商品
     public Product getProductById(Integer productId) {
 
         return productRepository.findById(productId).orElse(null);
-
     }
 
+    // 新增商品
     public Integer createProduct(ProductCreateRequest productCreateRequest) {
 
         Product product = new Product();
@@ -34,5 +39,36 @@ public class ProductService {
         return product.getProductId();
     }
 
+    // 更新商品
+    public Product updateProduct(Integer productId,ProductCreateRequest productCreateRequest) {
+
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if (optionalProduct.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "商品不存在，無法更新");
+        }
+
+        Product product = optionalProduct.get();
+        product.setProductName(productCreateRequest.getProductName());
+        product.setCategory(productCreateRequest.getCategory());
+        product.setImageUrl(productCreateRequest.getImageUrl());
+        product.setPrice(productCreateRequest.getPrice());
+        product.setStock(productCreateRequest.getStock());
+        product.setDescription(productCreateRequest.getDescription());
+        productRepository.save(product);
+
+        return product;
+    }
+
+    // 刪除商品
+    public void deleteProduct(Integer productId) {
+
+        if (!productRepository.existsById(productId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "找不到商品，無法刪除");
+        }
+
+        productRepository.deleteById(productId);
+
+    }
 
 }
